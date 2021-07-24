eba3ed88bfc891024bed25922d7ca02f410a3509
retroace <rajeshdpaudel@gmail.com>
2021-07-24 21:03:11 +0545

    Added clear all cache routes

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const CacheMiddleware = require('../middlewares/cacheMiddleware');
-
 const router = express.Router();
+
 const articleController = require('../controllers/articleController');
 const routeValidator = require('../middlewares/routeValidator');
 
+
 router.get('/article/:subject', routeValidator, CacheMiddleware, articleController.index);
-app.get('/clear-cache', cacheController.clearAll);
+router.get('/clear-cache', cacheController.clearAll);
 
 module.exports = router;
