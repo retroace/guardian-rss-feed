eb2832d4889c9dbe52442caeaa1c309a4891d16b
retroace <rajeshdpaudel@gmail.com>
2021-07-24 17:44:30 +0545

    Added express and initilized eslinter

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.status(200).json({ success: 'Rss feed' }));
+
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log(err);
+  // render the error page
+  res.status(err.status || 500).json({error : 'error'});
+});
+
+module.exports = app;
